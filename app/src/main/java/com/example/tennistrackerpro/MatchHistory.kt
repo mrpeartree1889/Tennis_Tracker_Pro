96779a816b7d467b15d5156e02b6e72816fb3d73
paulop <jan.voss@outlook.pt>
2019-09-17 15:36:28 -0500

    added landing page activity

---
+++ app/src/main/java/com/example/tennistrackerpro/MatchHistory.kt
@@ -0,0 +1,12 @@
+package com.example.tennistrackerpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchHistory : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_history)
+    }
+}
