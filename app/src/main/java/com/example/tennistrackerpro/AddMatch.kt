1eedec1ccb48bc071c496bed498a285e762f1dac
paulop <jan.voss@outlook.pt>
2019-09-17 18:31:42 -0500

    added add match activity and some buttons

---
+++ app/src/main/java/com/example/tennistrackerpro/AddMatch.kt
@@ -0,0 +1,12 @@
+package com.example.tennistrackerpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMatch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_match)
+    }
+}
