39d1e517c3a8b487b0e4f5d3953fbf643cfd25a9
paulop <jan.voss@outlook.pt>
2019-09-18 14:48:07 -0500

    finished statistics activity, started navigation

--- app/src/main/java/com/example/tennistrackerpro/MainActivity.kt
+++ app/src/main/java/com/example/tennistrackerpro/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.tennistrackerpro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        newMatchBtn.setOnClickListener() {
+            val intent = Intent(this, AddMatch::class.java)
+            startActivity(intent)
+        }
+
+        matchHistoryBtn.setOnClickListener() {
+            val intent = Intent(this, MatchHistory::class.java)
+            startActivity(intent)
+        }
+
+        statisticsBtn.setOnClickListener() {
+            val intent = Intent(this, Statistics::class.java)
+            startActivity(intent)
+        }
     }
 }
