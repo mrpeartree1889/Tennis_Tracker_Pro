c1025b25ea261591f99705e39dad1e833e54371f
paulop <jan.voss@outlook.pt>
2019-09-26 11:51:32 -0500

    added nagivation between all activities

--- app/src/main/java/com/example/tennistrackerpro/MatchHistory.kt
+++ app/src/main/java/com/example/tennistrackerpro/MatchHistory.kt
@@ -1,12 +1,25 @@
 package com.example.tennistrackerpro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_match.*
+import kotlinx.android.synthetic.main.activity_match_history.*
 
 class MatchHistory : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_history)
+
+        statisticsLayout.setOnClickListener() {
+            val intent = Intent(this, Statistics::class.java)
+            startActivity(intent)
+        }
+
+        newMatchLayout.setOnClickListener() {
+            val intent = Intent(this, AddMatch::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/tennistrackerpro/Statistics.kt
+++ app/src/main/java/com/example/tennistrackerpro/Statistics.kt
@@ -1,12 +1,24 @@
 package com.example.tennistrackerpro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_statistics.*
 
 class Statistics : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistics)
+
+        matchHistoryLayout.setOnClickListener() {
+            val intent = Intent(this, MatchHistory::class.java)
+            startActivity(intent)
+        }
+
+        newMatchLayout.setOnClickListener() {
+            val intent = Intent(this, AddMatch::class.java)
+            startActivity(intent)
+        }
     }
 }
